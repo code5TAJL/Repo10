c532aba78ce5274099778a0558f4d39fe6e6b953
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-06 13:56:59 +0200

    Added ex01/day10

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var myPet = {
+    species: "cat",
+    name: "Gina",
+    legs: 4,
+    friends: ["Leo", "Njuskalica"]
+}
+
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+
+module.exports = { myPet, myFunction };
