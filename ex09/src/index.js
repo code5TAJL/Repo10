32b8680722850190303393f4887a8383256351c5
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-07 10:44:48 +0200

    Added missing html files

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,11 +1,11 @@
 function myFunction(myObj, checkProp) {
     // Only change code below this line
-    myObj.hasOwnProperty(checkProp) 
+    if(myObj.hasOwnProperty(checkProp)) {
         return myObj[checkProp];
-    
+    }
     return "Not Found";
 
     // Only change code above this line
 }
-console.log(myFunction());
+
 module.exports = myFunction;
